1a879a09e1af74d112043d408ec78a4561627425
Ihor Podvalniy <bembasua@gmail.com>
2022-07-22 09:56:04 +0300

    чек

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsMarkupEl = createGallaryItemMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML(`beforeend`, galleryItemsMarkupEl);
+
+galleryEl.addEventListener(`click`, onGalleryClick);
+
+function createGallaryItemMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join(``);
+}
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains(`gallery__image`)) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${e.target.dataset.source}" width="800" height="600">
+    </div>
+`);
+  instance.show();
+  window.addEventListener(`keydown`, (event) => {
+    instance.close();
+  });
+}
+
+// window.addEventListener(`keydown`, onModalEscape);
+
+// function onModalEscape(e) {
+//   instance.close();
+// }
